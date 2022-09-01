b79a019ab33352437ac38dd9ac484a5e3c962305
Bradley Horner <bahorner87@gmail.com>
2022-09-01 12:35:55 +0100

    adding FTST address and symbol for stage 3

--- components/StageComponents/StakeStage/Web3StakingInterface.tsx
+++ components/StageComponents/StakeStage/Web3StakingInterface.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react"
 import { toast } from "react-toastify";
 import { useWeb3Context } from "../../../context";
-import { Contract, SmartContractManager } from "../../../Utils";
+import { Contract, SmartContractManager, StakingContractManager, TokenContractManager } from "../../../Utils";
 import { StakingList } from "./components/StakingList";
 import { StakingInterface } from "./components/StakingInterface";
 
@@ -13,11 +13,34 @@ export const Web3StakingInterface = () => {
     const [ShowStakingInterface, setShowStakingInterface] = useState(false);
     const [selectedStake, setSelectedStake] = useState<string>();
     const [stakeAddresses, setStakeAddresses] = useState<string[]>([]);
-    const StakingContract = SmartContractManager('StakingDirectory' as Contract, _network).then((data) => { return data });
+    const [tokenAddress, setTokenAddress] = useState('');
+    const [tokenSymbol, setTokenSymbol] = useState('');
+
+    const StakingContract = SmartContractManager('StakingDirectory' as Contract, _network).then((data) => data);
+    const TokenContract_TST = TokenContractManager(tokenAddress, _network).then((data) => data);
 
     useEffect(() => {
         getPositions();
-    }, [address]);
+    }, [address, tokenAddress]);
+
+    useEffect(() => {
+        stakeAddresses.length > 0 && getTokenAddress(stakeAddresses[0]);
+    }, [stakeAddresses])
+
+    const getTokenAddress = async (stakeAddress:string) => {
+        const StakingContract = StakingContractManager(stakeAddress as Contract).then((data) => data);
+        const stakingContractInit = await (await StakingContract);
+        //@ts-ignore
+        stakingContractInit.methods.TST_ADDRESS().call().then((data:never) => setTokenAddress(data)).catch((error:never) => console.log('TST_ADDRESS error', error));
+
+        const TokenContract = await (await TokenContract_TST);
+
+        if(TokenContract && Object.keys(TokenContract).length !== 0) {
+        TokenContract.methods.symbol().call()
+        .then((data:never) => setTokenSymbol(data))
+        .catch((error:never) => console.log('error getting token symbol', error));
+        }
+    }
 
     const stakeSelectionClickHandler = (contractAddress:string) => {
         setSelectedStake(contractAddress);
@@ -37,6 +60,7 @@ export const Web3StakingInterface = () => {
 
    return !ShowStakingInterface ? (
     <>
+    <div className="container mx-auto w-8/12 p-4 convertInput">{tokenSymbol} address: {tokenAddress}</div>
     <div className="mx-auto w-8/12 p-5 pb-0 grid grid-cols-4 gap-1">
         <span>Staking Period</span>
         <span>Approx. Reward</span>
