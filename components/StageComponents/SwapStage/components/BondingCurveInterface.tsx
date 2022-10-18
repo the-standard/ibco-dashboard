385e2aacffc04eccfffa87009d9ef043098a0e09
Ewan Sheldon <ewansheldon@gmail.com>
2022-10-18 12:48:19 +0100

    removes chart label, makes current price point highlighted, adds color under curve up to current price (#12)

--- components/StageComponents/SwapStage/components/BondingCurveInterface.tsx
+++ components/StageComponents/SwapStage/components/BondingCurveInterface.tsx
@@ -8,10 +8,12 @@ import {
   LineElement,
   Title,
   Tooltip,
+  Filler
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 // @ts-ignore
 import { chartArray } from '../../../../public/data/chartData';
+import { BigNumber } from 'ethers';
 
 ChartJS.register(
   CategoryScale,
@@ -20,7 +22,17 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
+  Filler
 );
+
+const currentPrice = BigNumber.from('886005893718244220');
+const currentPriceFloat = parseFloat(currentPrice.toString()) / 1000000000000000000;
+
+const customRadius = (context:any) => {
+  let index = context.dataIndex;
+  let value = context.dataset.data[ index ];
+  return value === currentPriceFloat ? 5 : 0;
+}
 
 export const options = {
   responsive: true,
@@ -34,12 +46,18 @@ export const options = {
   elements: {
     line: {
       tension: 0.4,
+    },
+    point: {
+      radius : customRadius,
+      display: true,
+      backgroundColor: 'rgb(153, 249, 255)'
     }
-  }
+  },
+  events: []
 };
 
 const labels = new Array(chartArray.length).fill('Current sEuro Price');
-const ArrayElem = chartArray.splice(0, Math.ceil(chartArray.length))
+const ArrayElem = chartArray.splice(0, Math.ceil(chartArray.length));
 
 export const data = {
   labels,
@@ -47,8 +65,16 @@ export const data = {
     {
       data: ArrayElem.map((dataPoint:string) => (parseFloat(dataPoint) / 1000000000000000000)),
       borderColor: 'rgb(153, 249, 255)',
-      backgroundColor: 'rgb(153, 249, 255)',
+      fill: false
     },
+    {
+      data: ArrayElem.filter((dataPoint:string) => BigNumber.from(dataPoint).lte(currentPrice)).map((dataPoint:string) => (parseFloat(dataPoint) / 1000000000000000000)),
+      backgroundColor: 'rgb(153, 249, 255, 0.6)',
+      fill: true,
+      point: {
+        display: false
+      }
+    }
   ],
 };
 
