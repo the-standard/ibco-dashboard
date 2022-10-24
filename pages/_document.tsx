63f545e0dd8fca0ee704278c50b402185c389ee2
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-10-24 13:50:00 +0100

    fix: fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
