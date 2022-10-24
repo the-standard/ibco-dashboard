5b139142d43cee9dbd36b7d102abd18bfde53c26
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-10-24 14:28:36 +0100

    fix: fonts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: false,
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+        />
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
       </Head>
       <body>
