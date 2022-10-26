e9564dd7a01efa00196a63c43df084765daa0e58
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-10-26 14:07:39 +0100

    adds express

--- server/index.js
+++ server/index.js
@@ -1,34 +1,40 @@
-const express = require('express')
-const next = require('next')
-const morgan = require('morgan')
+const express = require('express');
+const next = require('next');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
 
 const port = process.env.NODE_PORT || 8080;
 
-app.prepare()
-.then(() => {
-  const server = express()
+if (process.env.NODE_ENV === 'production') {
+  morgan.token('remote-addr', function (req, res) { return req.headers['cf-connecting-ip'] });
+}
 
-  server.use(morgan('combined'))
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(helmet.frameguard({ action: 'sameorigin' }));
+
+  server.use(morgan('combined'));
   
   server.get('/_health', (req, res) => {
-    res.sendStatus(200)
+    res.sendStatus(200);
   })
 
   server.get('*', (req, res) => {
-    return handle(req, res)
+    return handle(req, res);
   })
 
   server.listen(port, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
   })
 })
 .catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
+  console.error(ex.stack);
+  process.exit(1);
 })
