6c59c168e45309d54a7432500877c459f44d7241
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-10-26 13:49:38 +0100

    adds express

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const next = require('next')
+const morgan = require('morgan')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+
 const handle = app.getRequestHandler()
+
+const port = process.env.NODE_PORT || 8080;
 
 app.prepare()
 .then(() => {
   const server = express()
 
+  server.use(morgan('combined'))
+  
   server.get('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
