2f6d63cb35f7a06a931255cef769a9aa5e93f9e7
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-11-24 16:42:28 +0000

    adds docs

--- components/shared/footerLinks.tsx
+++ components/shared/footerLinks.tsx
@@ -10,7 +10,8 @@ export const FooterLinks = () => {
             <a href="https://www.thestandard.io/faq" rel="noreferrer" target="_blank">FAQ</a>
             <a href="https://www.thestandard.io/whitepaper" rel="noreferrer" target="_blank">Whitepaper</a>
             <a href="https://blog.thestandard.io" rel="noreferrer" target="_blank">Blog</a>
-            <a href="https://app.gitbook.com/o/XzmVI0dw0DD34ihkKq80/home" rel="noreferrer" target="_blank">Documentation</a>
+            <a href="https://docs.ibco-general.thestandard.io/" rel="noreferrer" target="_blank">General Docs</a>
+            <a href="https://docs.ibco-technical.thestandard.io/" rel="noreferrer" target="_blank">Technical Docs</a>
         </StyledFooterNavContainer>
     )
 };
