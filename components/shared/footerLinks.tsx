807535c981b9a4f5e4db19eca189823a2bb06286
Simon Morley <309336+simonmorley@users.noreply.github.com>
2022-11-24 16:44:45 +0000

    adds docs

--- components/shared/footerLinks.tsx
+++ components/shared/footerLinks.tsx
@@ -4,14 +4,12 @@ import { StyledFooterNavContainer } from "./uiElements/styles/FooterLinksStyles"
 export const FooterLinks = () => {
     return (
         <StyledFooterNavContainer>
-            <a href="https://www.thestandard.io/" rel="noreferrer" target="_blank">The Standard Protocol</a>
-            <a href="https://www.thestandard.io/ecosystem" rel="noreferrer" target="_blank">Ecosystem</a>
-            <a href="https://www.thestandard.io/careers" rel="noreferrer" target="_blank">Careers</a>
             <a href="https://www.thestandard.io/faq" rel="noreferrer" target="_blank">FAQ</a>
             <a href="https://www.thestandard.io/whitepaper" rel="noreferrer" target="_blank">Whitepaper</a>
             <a href="https://blog.thestandard.io" rel="noreferrer" target="_blank">Blog</a>
             <a href="https://docs.ibco-general.thestandard.io/" rel="noreferrer" target="_blank">General Docs</a>
             <a href="https://docs.ibco-technical.thestandard.io/" rel="noreferrer" target="_blank">Technical Docs</a>
+            <a href="https://discord.gg/THWyBQ4RzQ" rel="noreferrer" target="_blank">Support</a>
         </StyledFooterNavContainer>
     )
 };
